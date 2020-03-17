7c0d37acb1510c8d1c06587cb21682417a23c8e6
Kenny Lam <kinwai.lam730@gmail.com>
2020-03-17 16:33:56 -0400

    Added onupgradeneeded path

--- public/db.js
+++ public/db.js
@@ -1,3 +1,12 @@
 // add code for indexedDB here and add a script tag for this file above index.js in index.html
-const addBtn = document.getElementById("add-btn");
-const subBtn = document.getElementById("sub-btn");
+let db;
+//request opens a indexed database called "budget" with a version number of 1
+const request  = indexedDB.open("budget", 1);
+
+//request.onupgradeneeded asks what version of the indexDatabse we are on, depending on version we can do if statements if(event.oldVersion < (version number), you can create more indexes within a database or a new database.
+request.onupgradeneeded = function(event){
+  const db = event.target.result;
+  //createObjectStore creates a db index of "pending" with autoincrement
+  db.createObjectStore("pending", { autoincrement: true});
+}
+
