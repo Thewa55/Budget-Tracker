191b28d3332be101e3027899abc49f72cd49177e
Kenny Lam <kinwai.lam730@gmail.com>
2020-03-17 16:11:25 -0400

    Changed the service worker files_to_cache and renamed the manfest

--- public/db.js
+++ public/db.js
@@ -1,1 +1,3 @@
 // add code for indexedDB here and add a script tag for this file above index.js in index.html
+const addBtn = document.getElementById("add-btn");
+const subBtn = document.getElementById("sub-btn");
